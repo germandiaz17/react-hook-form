ed5e155c245be0864adee4c18719b4d5a30fa8d4
German Diaz <germanrodrigo.2003@hotmail.com>
2021-07-12 21:37:21 -0500

    despliege

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from 'react-hook-form'
 
 function App() {
+  
+  const{handleSubmit, register} = useForm()
+
+  const callBack = (formValues) => {
+    const name = JSON.stringify(formValues.name)
+    const lastName = JSON.stringify(formValues.lastName)
+    const age = JSON.stringify(formValues.age)
+    const email = JSON.stringify(formValues.email)
+    const country = JSON.stringify(formValues.country)
+    alert(
+      `
+      Name: ${name}
+      Last Name: ${lastName}
+      Age: ${age}
+      Email: ${email}
+      Country: ${country}
+      `
+    )
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit(callBack)}>
+          <div>
+            <div>
+              <label>
+                Name:
+                <input type="text" {...register('name')}/>
+              </label>
+            </div>
+            <div>
+              <label>
+                last name:
+                <input type="text" {...register('lastName')}/>
+              </label>
+            </div>
+            <div>
+            <label>
+                Age:
+                <input type="number" {...register('age')}/>
+              </label>
+            </div>
+            <div>
+            <label>
+                Email:
+                <input type="email" {...register('email')}/>
+              </label>
+            </div>
+            <div>
+            <label>
+                Country:
+                <input type="text" {...register('country')}/>
+              </label>
+            </div>
+            <div>
+              <button type='submit'>Enviar</button>
+            </div>
+          </div>
+        </form>
       </header>
     </div>
   );
